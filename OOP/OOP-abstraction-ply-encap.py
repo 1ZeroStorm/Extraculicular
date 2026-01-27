0ff267c30b7d9e53cd1dfc1e596fdfc57d2e4277
1ZeroStorm <162772235+1ZeroStorm@users.noreply.github.com>
2026-01-27 15:11:20 +0700

    updated test

--- OOP/OOP-abstraction-ply-encap.py
+++ OOP/OOP-abstraction-ply-encap.py
@@ -28,7 +28,7 @@ class Character(ABC):
 # === Polymorphism ===
 class Warrior(Character):
     def attack(self):
-        print(f"{self.name} swings a mighty sword!")
+        print(f"{self.name} swings a sword!")
         return 15
 
     def defend(self, damage):
@@ -47,8 +47,38 @@ class Mage(Character):
         self.set_health(self.get_health() - reduced)
         print(f"{self.name} uses magic barrier, health now {self.get_health()}")
 
-
 class Archer(Character):
+    def attack(self):
+        print(f"{self.name} shoots an arrow!")
+        return 10
+
+    def defend(self, damage):
+        self.set_health(self.get_health() - damage)
+        print(f"{self.name} dodges but still takes damage, health now {self.get_health()}")
+
+class createGaldiatorArena:
+    def __init__(self, *character):
+        self.characters = list(character)
+
+    def startwar(self):
+        for chara in self.characters:
+            for enemy in self.characters:
+                if chara != enemy:
+                    while chara.get_health() > 0 and enemy.get_health() > 0:
+                        dmg = chara.attack()
+                        enemy.defend(dmg)
+                        if enemy.get_health() <= 0:
+                            print(f"{enemy.name} has fallen! {chara.name} wins!")
+                            break
+
+                        dmg = enemy.attack()
+                        chara.defend(dmg)
+                        if chara.get_health() <= 0:
+                            print(f"{chara.name} has fallen! {enemy.name} wins!")
+                            break
+
+
+class Dragon(Character):
     def attack(self):
         print(f"{self.name} shoots an arrow!")
         return 10
@@ -59,20 +89,17 @@ class Archer(Character):
 
 
 # === Game Simulation ===
-def battle(c1, c2):
+def battle(c1, c2): 
     print(f"\nBattle: {c1.name} vs {c2.name}")
     while c1.get_health() > 0 and c2.get_health() > 0:
-        
          
         dmg1 = c1.attack() # c1 attacks c2
         c2.defend(dmg1) # c2 defends against damage from c1
 
-        
         if c2.get_health() <= 0:
             print(f"{c2.name} has fallen! {c1.name} wins!")
             break
         
-
         dmg2 = c2.attack() # c2 attacks c1
         c1.defend(dmg2) # c1 defends against damage from c2
 
@@ -82,10 +109,9 @@ def battle(c1, c2):
 
 
 # === Example Run ===
-warrior = Warrior("Thorin", 100)
-mage = Mage("Gandalf", 80)
-archer = Archer("Legolas", 90)
-
+warrior = Warrior("Guts (warrior)", 100)
+mage = Mage("Jinwoo (mage)", 80)
+archer = Archer("Sukuna (archer)", 90)
 
 battle(warrior, mage)
 battle(mage, archer)
